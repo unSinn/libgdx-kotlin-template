d4ef7caa5cad2da622065e1fe3bbdb8673cf5760
Fabian Mächler <fabian.maechler@gmail.com>
2021-01-16 20:29:17 +0100

    Update, getting it to work again

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # libgdx-kotlin-template
 
-This is a template for LibGDX using Kotlin,  and [gradle-script-kotlin](https://github.com/gradle/gradle-script-kotlin). It also has [KTX](https://github.com/libktx/ktx) dependencies. It currently only has the core and desktop modules. 
+This is a template for LibGDX using Kotlin,  and [gradle-script-kotlin](https://github.com/gradle/gradle-script-kotlin).
+It also has [KTX](https://github.com/libktx/ktx) dependencies. It currently only has the core and desktop modules. 
 
-I created this because the official LibGDX project generator is still using an old version of gradle and old tasks, I didn't like the project structure it made, it didn't automatically add Kotlin support, and it was using the Groovy Gradle DSL, and I prefer the new Kotlin Gradle DSL.
+I created this because the official LibGDX project generator is still using an old version of gradle and old tasks, 
+I didn't like the project structure it made, it didn't automatically add Kotlin support, and it was using the Groovy Gradle DSL, and I prefer the new Kotlin Gradle DSL.
 
 To use it, simply download the repo and start coding. You can import the project into IntelliJ using the settings.gradle file. To run the desktop game, simply run the gradle task :desktop:run.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,59 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-buildscript {
-
-    repositories {
-        jcenter()
-    }
-    dependencies {
-        classpath("com.github.jengelman.gradle.plugins:shadow:2.0.4")
-    }
+plugins {
+    idea
+    kotlin("jvm") version "1.4.21"
 }
 
 allprojects {
-
-
-    repositories {
-        jcenter()
-    }
-
-    var gdxVersion: String by extra
-    var ktxVersion: String by extra
-
-    gdxVersion = "1.9.8"
-    ktxVersion = "1.9.8-b3"
-
-    group = "com.example.gdxgame"
     version = "1.0"
 
-    apply(plugin = "kotlin")
+    val appName by extra { "Gorcassonne" }
+    val gdxVersion by extra { "1.9.13" }
 
-    tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+        maven { url = uri("https://oss.sonatype.org/content/repositories/releases/") }
     }
 }
 
 
-plugins {
-    base
-    kotlin("jvm") version "1.2.50"
+
+project(":desktop") {
+    apply(plugin = "kotlin")
+
+    val gdxVersion = this.extra["gdxVersion"]
+
+    dependencies {
+        compile(project(":core"))
+        compile("com.badlogicgames.gdx:gdx-backend-lwjgl:$gdxVersion")
+        compile("com.badlogicgames.gdx:gdx-platform:$gdxVersion:natives-desktop")
+        compile("com.badlogicgames.gdx:gdx-freetype-platform:$gdxVersion:natives-desktop")
+        compile("com.badlogicgames.gdx:gdx-tools:$gdxVersion")
+        compile("com.badlogicgames.gdx:gdx-controllers-desktop:$gdxVersion")
+        compile("com.badlogicgames.gdx:gdx-controllers-platform:$gdxVersion:natives-desktop")
+        compile(kotlin("stdlib"))
+    }
 }
 
-dependencies {
-    // Make the root project archives configuration depend on every subproject
-    subprojects.forEach {
-        archives(it)
+project(":core") {
+    apply(plugin = "kotlin")
+
+    val gdxVersion = this.extra["gdxVersion"]
+
+    dependencies {
+        val ktxVersion = "1.9.13-b1"
+
+        compile("de.alsclo:voronoi-java:1.0")
+        compile("com.badlogicgames.gdx:gdx:$gdxVersion")
+        compile("com.badlogicgames.gdx:gdx-freetype:$gdxVersion")
+        compile("com.badlogicgames.gdx:gdx-controllers:$gdxVersion")
+        compile(kotlin("stdlib"))
+        compile(group = "io.github.libktx", name = "ktx-app", version = ktxVersion)
+        compile(group = "io.github.libktx", name = "ktx-inject", version = ktxVersion)
+        compile(group = "io.github.libktx", name = "ktx-log", version = ktxVersion)
+        compile(group = "io.github.libktx", name = "ktx-assets", version = ktxVersion)
+        compile(group = "io.github.libktx", name = "ktx-freetype", version = ktxVersion)
+        compile(group = "io.github.libktx", name = "ktx-graphics", version = ktxVersion)
     }
 }
