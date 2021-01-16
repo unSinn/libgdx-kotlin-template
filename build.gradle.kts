ca72d60f04364c0dba023d5a5f05b71e83442b86
Fabian Mächler <fabian.maechler@gmail.com>
2021-01-16 20:49:27 +0100

    fix deprecated "implementation" keyword in kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,22 +17,28 @@ allprojects {
     }
 }
 
-
-
 project(":desktop") {
     apply(plugin = "kotlin")
 
     val gdxVersion = this.extra["gdxVersion"]
 
+    // Use this task to run the game if IntelliJ run application configuration doesn't work.
+    tasks.register<JavaExec>("run") {
+        main = "desktop.Main"
+        classpath = sourceSets.main.get().runtimeClasspath
+        standardInput = System.`in`
+        isIgnoreExitValue = true
+    }
+
     dependencies {
-        compile(project(":core"))
-        compile("com.badlogicgames.gdx:gdx-backend-lwjgl:$gdxVersion")
-        compile("com.badlogicgames.gdx:gdx-platform:$gdxVersion:natives-desktop")
-        compile("com.badlogicgames.gdx:gdx-freetype-platform:$gdxVersion:natives-desktop")
-        compile("com.badlogicgames.gdx:gdx-tools:$gdxVersion")
-        compile("com.badlogicgames.gdx:gdx-controllers-desktop:$gdxVersion")
-        compile("com.badlogicgames.gdx:gdx-controllers-platform:$gdxVersion:natives-desktop")
-        compile(kotlin("stdlib"))
+        implementation(project(":core"))
+        implementation("com.badlogicgames.gdx:gdx-backend-lwjgl:$gdxVersion")
+        implementation("com.badlogicgames.gdx:gdx-platform:$gdxVersion:natives-desktop")
+        implementation("com.badlogicgames.gdx:gdx-freetype-platform:$gdxVersion:natives-desktop")
+        implementation("com.badlogicgames.gdx:gdx-tools:$gdxVersion")
+        implementation("com.badlogicgames.gdx:gdx-controllers-desktop:$gdxVersion")
+        implementation("com.badlogicgames.gdx:gdx-controllers-platform:$gdxVersion:natives-desktop")
+        implementation(kotlin("stdlib"))
     }
 }
 
@@ -44,16 +50,16 @@ project(":core") {
     dependencies {
         val ktxVersion = "1.9.13-b1"
 
-        compile("de.alsclo:voronoi-java:1.0")
-        compile("com.badlogicgames.gdx:gdx:$gdxVersion")
-        compile("com.badlogicgames.gdx:gdx-freetype:$gdxVersion")
-        compile("com.badlogicgames.gdx:gdx-controllers:$gdxVersion")
-        compile(kotlin("stdlib"))
-        compile(group = "io.github.libktx", name = "ktx-app", version = ktxVersion)
-        compile(group = "io.github.libktx", name = "ktx-inject", version = ktxVersion)
-        compile(group = "io.github.libktx", name = "ktx-log", version = ktxVersion)
-        compile(group = "io.github.libktx", name = "ktx-assets", version = ktxVersion)
-        compile(group = "io.github.libktx", name = "ktx-freetype", version = ktxVersion)
-        compile(group = "io.github.libktx", name = "ktx-graphics", version = ktxVersion)
+        implementation(kotlin("stdlib"))
+        implementation("de.alsclo:voronoi-java:1.0")
+        implementation("com.badlogicgames.gdx:gdx:$gdxVersion")
+        implementation("com.badlogicgames.gdx:gdx-freetype:$gdxVersion")
+        implementation("com.badlogicgames.gdx:gdx-controllers:$gdxVersion")
+        api(group = "io.github.libktx", name = "ktx-app", version = ktxVersion)
+        implementation(group = "io.github.libktx", name = "ktx-inject", version = ktxVersion)
+        implementation(group = "io.github.libktx", name = "ktx-log", version = ktxVersion)
+        implementation(group = "io.github.libktx", name = "ktx-assets", version = ktxVersion)
+        implementation(group = "io.github.libktx", name = "ktx-freetype", version = ktxVersion)
+        implementation(group = "io.github.libktx", name = "ktx-graphics", version = ktxVersion)
     }
 }
--- desktop/src/main/kotlin/desktop/Main.kt
+++ desktop/src/main/kotlin/desktop/Main.kt
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 import core.MainGame
 
-fun main(arg: Array<String>) {
+fun main() {
     val config = LwjglApplicationConfiguration()
     config.height = 1080
     config.width = 1920
